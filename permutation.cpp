d03cc78a5a1256e58293e5cad8da1067fb1c0f5a
Srihit-489 <71779278+Srihit-489@users.noreply.github.com>
2021-03-26 20:00:03 +0530

    Add files via upload

---
+++ permutation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	int i=2;
+	while(i <= n){
+		cout<<i<<" ";
+		i = i + 2;
+	}
+	i=1;
+	while(i <= n){
+		cout<<i<<" ";
+		i = i + 2;
+	}
+	cout<<endl;
+	return 0;
+}
